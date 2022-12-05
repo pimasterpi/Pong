aeaa4a569cbf74e0c8cc4a61db49078ee1540e6b
pimasterpi <lucas.indy@gmail.com>
2022-12-05 10:09:27 -0800

    Clean Up Code & Add Bounds to Paddle Movement

--- Pong.java
+++ Pong.java
@@ -6,7 +6,7 @@ import java.awt.event.*;
 public class Pong extends JComponent implements Runnable {
     Image image;
     Graphics2D graphics2D;
-    private int verticalPosition = 0;
+    int verticalPosition = 0;
 
     Pong draw;
 
@@ -22,25 +22,16 @@ public class Pong extends JComponent implements Runnable {
 
         JFrame frame = new JFrame("Pong") {{
             setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-            setSize(600, 600);
+            setSize(600, 400);
             setLocationRelativeTo(null);
             setVisible(true);
         }};
 
         Container content = frame.getContentPane();
+        content.setSize(600, 400);
         content.setLayout(new BorderLayout());
         draw = new Pong();
         content.add(draw, BorderLayout.CENTER);
-
-        JLabel yPosition = new JLabel() {{
-            setText("yPosition: " + verticalPosition);
-        }};
-
-        JPanel panel = new JPanel() {{
-            add(yPosition);
-        }};
-
-        content.add(panel, BorderLayout.NORTH);
 
         frame.addKeyListener(new KeyAdapter() {
             @Override
@@ -52,12 +43,14 @@ public class Pong extends JComponent implements Runnable {
             public void keyPressed(KeyEvent e) {
                 switch (e.getKeyCode()) {
                     case KeyEvent.VK_UP, KeyEvent.VK_LEFT -> {
-                        yPosition.setText("yPosition: " + (verticalPosition -= 5));
-                        draw.paddleUp(verticalPosition);
+                        if (verticalPosition > 0) verticalPosition -= 5;
+                        draw.paddleMove(verticalPosition);
+                        System.out.println(verticalPosition);
                     }
                     case KeyEvent.VK_DOWN, KeyEvent.VK_RIGHT -> {
-                        yPosition.setText("yPosition: " + (verticalPosition += 5));
-                        draw.paddleDown(verticalPosition);
+                        if (verticalPosition < 265) verticalPosition += 5;
+                        draw.paddleMove(verticalPosition);
+                        System.out.println(verticalPosition);
                     }
                 }
             }
@@ -70,29 +63,12 @@ public class Pong extends JComponent implements Runnable {
 
     }
 
-    public void paddleUp(int verticalPosition) {
-        System.out.println(verticalPosition);
+    public void paddleMove(int verticalPosition) {
         graphics2D.setPaint(Color.white);
         graphics2D.fillRect(0, 0, getSize().width, getSize().height);
-        graphics2D.setStroke(new BasicStroke(5));
         graphics2D.setPaint(Color.black);
-        graphics2D.fillRect(0, verticalPosition, 15, verticalPosition + 90);
+        graphics2D.fillRect(0, verticalPosition, 15, 100);
 
-
-        graphics2D.fillRect(30, 90, 45, 180);
-        graphics2D.fillRect(60, 180, 75, 270);
-
-
-        repaint();
-    }
-
-    public void paddleDown(int verticalPosition) {
-        System.out.println(verticalPosition);
-        graphics2D.setPaint(Color.white);
-        graphics2D.fillRect(0, 0, getSize().width, getSize().height);
-        graphics2D.setStroke(new BasicStroke(5));
-        graphics2D.setPaint(Color.black);
-        graphics2D.fillRect(0, verticalPosition, 15, verticalPosition + 90);
         repaint();
     }
 
