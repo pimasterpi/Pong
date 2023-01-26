221c4bd5e2f9c1bf71a6790a01794da833b3fab4
pimasterpi <lucas.indy@gmail.com>
2023-01-26 07:51:19 -0800

    Add Time System & Ball Movement/Physics

--- Pong.java
+++ Pong.java
@@ -6,9 +6,41 @@ import java.awt.event.*;
 public class Pong extends JComponent implements Runnable {
     Image image;
     Graphics2D graphics2D;
-    int verticalPosition = 0;
+    int verticalPositionBar = 0;
+    int horizontalPositionBall = 0;
+    int verticalPositionBall = 0;
 
+    double horizontalVelocityBall = 5;
+    double verticalVelocityBall = 5;
     Pong draw;
+
+    Timer timer;
+
+    ActionListener taskPerformer = new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+            if (e.getSource() == timer) {
+
+                draw.verticalPositionBall += draw.verticalVelocityBall;
+                draw.horizontalPositionBall += draw.horizontalVelocityBall;
+
+                if (draw.verticalPositionBall <= 0 && draw.verticalVelocityBall < 0 || draw.verticalPositionBall >= 355 && draw.verticalVelocityBall > 0) {
+                    draw.verticalVelocityBall *= -1;
+                }
+
+                if (draw.horizontalPositionBall <= 0 && draw.horizontalVelocityBall < 0 || draw.horizontalPositionBall >= 580 && draw.horizontalVelocityBall > 0) {
+                    draw.horizontalVelocityBall *= -1;
+                }
+                if (draw.horizontalPositionBall <= 15 && draw.horizontalVelocityBall < 0 && (draw.verticalPositionBar - draw.verticalPositionBall <= 10 && draw.verticalPositionBall - draw.verticalPositionBar - 100 <= 0)) {
+                    draw.horizontalVelocityBall *= -1;
+                }
+
+
+
+                draw.refresh();
+                timer.restart();
+            }
+        }
+    };
 
 
     public static void main(String[] args) {
@@ -33,6 +65,10 @@ public class Pong extends JComponent implements Runnable {
         draw = new Pong();
         content.add(draw, BorderLayout.CENTER);
 
+        timer = new Timer(10, taskPerformer);
+        timer.setRepeats(true);
+        timer.start();
+
         frame.addKeyListener(new KeyAdapter() {
             @Override
             public void keyTyped(KeyEvent e) {
@@ -43,14 +79,12 @@ public class Pong extends JComponent implements Runnable {
             public void keyPressed(KeyEvent e) {
                 switch (e.getKeyCode()) {
                     case KeyEvent.VK_UP, KeyEvent.VK_LEFT -> {
-                        if (verticalPosition > 0) verticalPosition -= 5;
-                        draw.paddleMove(verticalPosition);
-                        System.out.println(verticalPosition);
+                        if (draw.verticalPositionBar > 0) draw.verticalPositionBar -= 5;
+                        draw.refresh();
                     }
                     case KeyEvent.VK_DOWN, KeyEvent.VK_RIGHT -> {
-                        if (verticalPosition < 265) verticalPosition += 5;
-                        draw.paddleMove(verticalPosition);
-                        System.out.println(verticalPosition);
+                        if (draw.verticalPositionBar < 265) draw.verticalPositionBar += 5;
+                        draw.refresh();
                     }
                 }
             }
@@ -61,17 +95,19 @@ public class Pong extends JComponent implements Runnable {
             }
         });
 
+
     }
 
-    public void paddleMove(int verticalPosition) {
+
+    public void refresh() {
         graphics2D.setPaint(Color.white);
         graphics2D.fillRect(0, 0, getSize().width, getSize().height);
         graphics2D.setPaint(Color.black);
-        graphics2D.fillRect(0, verticalPosition, 15, 100);
+        graphics2D.fillRect(0, verticalPositionBar, 15, 100);
+        graphics2D.fillOval(horizontalPositionBall, verticalPositionBall, 10, 10);
 
         repaint();
     }
-
 
     protected void paintComponent(Graphics g) {
         if (image == null) {
